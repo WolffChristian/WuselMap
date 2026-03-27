a2925a29f251eff7f62c8a5c971eb437d5fa5b91
WolffChristian <253496176+WolffChristian@users.noreply.github.com>
2026-03-27 10:41:10 +0100

    Update auth_manager.py

--- auth_manager.py
+++ auth_manager.py
@@ -2,11 +2,11 @@ import streamlit as st
 from database_manager import hash_passwort, hole_df, ausfuehren
 
 def login_bereich():
-    t1, t2, t3 = st.tabs(["🔑 Login", "📝 Registrierung", "❓ Hilfe"])
+    t1, t2, t3 = st.tabs(["🔑 Login", "📝 Registrierung", "❓ Passwort vergessen"])
     
     with t1:
-        u = st.text_input("Nutzername", key="l_u")
-        p = st.text_input("Passwort", type="password", key="l_p")
+        u = st.text_input("Nutzername", key="login_user")
+        p = st.text_input("Passwort", type="password", key="login_pass")
         if st.button("Anmelden", width='stretch'):
             hashed_p = hash_passwort(p)
             df = hole_df("SELECT nutzer_id, rolle FROM nutzer WHERE benutzername=%s AND passwort=%s", (u, hashed_p))
@@ -15,35 +15,40 @@ def login_bereich():
                 st.session_state.nutzer_id = int(df.iloc[0]['nutzer_id'])
                 st.session_state.rolle = df.iloc[0]['rolle']
                 st.rerun()
-            else: st.error("Nutzername oder Passwort falsch.")
+            else:
+                st.error("Zugangsdaten nicht korrekt.")
 
     with t2:
-        with st.form("reg_form"):
+        with st.form("registrierung"):
+            st.write("Neues Konto erstellen")
             reg_u = st.text_input("Wunsch-Nutzername")
-            reg_p = st.text_input("Sicheres Passwort", type="password")
+            reg_p = st.text_input("Passwort", type="password")
             reg_m = st.text_input("E-Mail Adresse")
             reg_v = st.text_input("Vorname")
             reg_n = st.text_input("Nachname")
-            reg_a = st.number_input("Alter", 0, 120, 30)
-            reg_agb = st.checkbox("Ich akzeptiere die AGB & Datenschutzbestimmungen")
-            if st.form_submit_button("Konto erstellen", width='stretch'):
-                if reg_agb and all([reg_u, reg_p, reg_m]):
+            reg_a = st.number_input("Alter", 0, 120, 25)
+            reg_agb = st.checkbox("Ich akzeptiere die Datenschutzbestimmungen")
+            
+            if st.form_submit_button("Registrierung abschließen"):
+                if reg_agb and all([reg_u, reg_p, reg_m, reg_v, reg_n]):
                     hashed_p = hash_passwort(reg_p)
                     sql = "INSERT INTO nutzer (benutzername, passwort, email, vorname, nachname, alter_wert, rolle) VALUES (%s,%s,%s,%s,%s,%s,'user')"
                     if ausfuehren(sql, (reg_u, hashed_p, reg_m, reg_v, reg_n, reg_a)):
-                        st.success("Konto erstellt! Du kannst dich jetzt einloggen.")
+                        st.success("Konto erfolgreich erstellt! Du kannst dich jetzt einloggen.")
                         st.balloons()
-                else: st.warning("Bitte alle Pflichtfelder ausfüllen.")
+                else:
+                    st.warning("Bitte fülle alle Felder aus und akzeptiere die Bestimmungen.")
 
     with t3:
-        st.subheader("Passwort vergessen?")
-        with st.form("reset_form"):
+        st.write("Passwort zurücksetzen")
+        with st.form("reset"):
             res_u = st.text_input("Nutzername")
-            res_m = st.text_input("Deine E-Mail")
-            new_p = st.text_input("Neues Passwort wählen", type="password")
-            if st.form_submit_button("Passwort zurücksetzen"):
+            res_m = st.text_input("Registrierte E-Mail")
+            new_p = st.text_input("Neues Passwort", type="password")
+            if st.form_submit_button("Passwort aktualisieren"):
                 df = hole_df("SELECT nutzer_id FROM nutzer WHERE benutzername=%s AND email=%s", (res_u, res_m))
                 if not df.empty:
                     if ausfuehren("UPDATE nutzer SET passwort=%s WHERE benutzername=%s", (hash_passwort(new_p), res_u)):
-                        st.success("Passwort wurde erfolgreich aktualisiert!")
-                else: st.error("Kombination aus Name und E-Mail nicht gefunden.")
+                        st.success("Dein Passwort wurde sicher geändert.")
+                else:
+                    st.error("Diese Kombination ist uns nicht bekannt.")
